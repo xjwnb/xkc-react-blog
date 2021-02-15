023a2ff494dfc3583a1aea234d07b8a0e4925b1e
1565163445@qq.com <1565163445@qq.com>
2021-02-16 01:08:25 +0800

    update 自适应样式

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-02-03 17:42:34
- * @LastEditTime: 2021-02-15 22:39:29
+ * @LastEditTime: 2021-02-16 00:25:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \xkc-react-blog\src\App.js
@@ -9,6 +9,7 @@
 // 布局
 import Layout from "@/layout";
 import { useEffect } from "react";
+import "./App.css";
 
 function App() {
   const fontSize = "16px";
